36703f300f8f7499733fa7d5b64e233a05d85c12
Paolo Missagia <login@paolomissagia.com>
2024-06-29 22:24:52 +0100

    fixed page height after increasing font

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
     const currentYear = new Date().getFullYear()
 
     return (
-        <footer className="border-t border-t-accent py-8">
-            <div className="flex flex-row justify-between pb-8">
+        <footer className="border-t border-t-accent py-8 text-xl">
+            <div className="flex flex-row justify-between">
                 <span>copyright &#169; {currentYear}</span>
                 <span>all rights reserverd</span>
             </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
-        <header className="container border-b border-b-accent py-8 text-lg">
+        <header className="container border-b border-b-accent py-8 text-xl">
             <nav className="flex flex-row justify-between">
-                <NavLink to="/">user@paolomissagia$</NavLink>
+                <NavLink to="/">user@paolomissagia<span className="text-secondary">$</span></NavLink>
                 <ul className="flex flex-row gap-5">
                     <NavLink to="/">home<span>/</span></NavLink>
                     <NavLink to="/projects">projects<span>/</span></NavLink>
