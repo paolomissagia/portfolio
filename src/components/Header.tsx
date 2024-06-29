800697ac796a6247a72e108e8e09a27320384828
Paolo Missagia <login@paolomissagia.com>
2024-06-29 21:19:59 +0100

    added home route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,9 @@ export default function Header() {
     return (
         <header className="container border-b border-b-accent py-8">
             <nav className="flex flex-row justify-between">
-                <div>
-                    pmissagia$
-                </div>
+                <span>pmissagia$</span>
                 <ul className="flex flex-row gap-5">
+                    <li><a href="/">Home</a></li>
                     <li><a href="/projects">Projects</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/contact">Contact</a></li>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <p>
-                AstroPaper is a minimal, responsive, accessible and SEO-friendly Astro blog theme. This theme follows best practices and provides accessibility out of the box. Light and dark mode are supported by default. Moreover, additional color schemes can also be configured.
-            </p>
+            <h1>HOME</h1>
         </div>
     )
 }
