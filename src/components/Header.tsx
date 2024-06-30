2f5a8085f1279cba225282f4c51ad5a76f5e488c
Paolo Missagia <login@paolomissagia.com>
2024-06-30 10:05:06 +0100

    updated navbar to accomodate button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
 import { NavLink } from "react-router-dom";
+import HamburgerIcon from "../assets/icons/HamburgerIcon";
 
 export default function Header() {
     return (
-        <header className="container border-b border-b-accent py-8 text-xl">
-            <nav className="flex flex-row justify-between">
-                <NavLink to="/">user@paolomissagia<span className="text-secondary">$</span></NavLink>
-                <ul className="flex flex-row gap-5">
+        <header className="border-b border-b-accent py-8 text-xl flex flex-row justify-between">
+            <NavLink to="/">user@paolomissagia<span className="text-secondary">$</span></NavLink>
+            <nav className="max-h-[24px]">
+                <ul className="hidden sm:flex flex-row gap-5">
                     <NavLink to="/">home<span>/</span></NavLink>
                     <NavLink to="/projects">projects<span>/</span></NavLink>
                     <NavLink to="/contact">contact<span>/</span></NavLink>
                 </ul>
+                <button className="sm:hidden">
+                    <HamburgerIcon />
+                </button>
             </nav>
         </header>
     )
