3e9959efb588db0766e97d6f3e96d5ef8bead255
Paolo Missagia <login@paolomissagia.com>
2024-06-30 10:04:55 +0100

    added navbar icons

---
+++ src/assets/icons/CloseIcon.tsx
@@ -0,0 +1,19 @@
+export default function CloseIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            className="menu-icon"
+            viewBox="0 0 24 24"
+        >
+            <path d="M18 6L6 18" className="close"></path>
+            <path d="M6 6L18 18" className="close"></path>
+        </svg>
+    );
+}
