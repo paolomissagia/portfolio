468024e8ab9c0c6564872ffabe689204dda70393
Paolo Missagia <login@paolomissagia.com>
2024-06-30 11:25:14 +0100

    added mobile view for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,33 @@
 import { NavLink } from "react-router-dom";
-import HamburgerIcon from "../assets/icons/HamburgerIcon";
+import { useState } from "react";
+import { FaHamburger, FaRegWindowClose } from "react-icons/fa";
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenuOpen = () => setIsMenuOpen(!isMenuOpen);
+
     return (
-        <header className="border-b border-b-accent py-8 text-xl flex flex-row justify-between">
-            <NavLink to="/">user@paolomissagia<span className="text-secondary">$</span></NavLink>
-            <nav className="max-h-[24px]">
-                <ul className="hidden sm:flex flex-row gap-5">
-                    <NavLink to="/">home<span>/</span></NavLink>
-                    <NavLink to="/projects">projects<span>/</span></NavLink>
-                    <NavLink to="/contact">contact<span>/</span></NavLink>
-                </ul>
-                <button className="sm:hidden">
-                    <HamburgerIcon />
+        <header className="border-b border-b-accent text-xl flex flex-col sm:flex-row justify-between items-center">
+            <div className="flex justify-between items-center w-full sm:w-auto py-4 sm:py-8">
+                <NavLink to="/">user@paolomissagia<span className="text-secondary">$</span></NavLink>
+                <button className="sm:hidden" onClick={toggleMenuOpen}>
+                    {isMenuOpen ? <FaRegWindowClose size={"1.2em"} /> : <FaHamburger size={"1.2em"} />}
                 </button>
+            </div>
+            <nav className={`w-full sm:w-auto ${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
+                <ul className="flex flex-col sm:flex-row sm:gap-5 items-center sm:items-stretch">
+                    <li>
+                        <NavLink to="/" onClick={() => setIsMenuOpen(false)}>home<span>/</span></NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/projects" onClick={() => setIsMenuOpen(false)}>projects<span>/</span></NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact" onClick={() => setIsMenuOpen(false)}>contact<span>/</span></NavLink>
+                    </li>
+                </ul>
             </nav>
         </header>
-    )
+    );
 }
