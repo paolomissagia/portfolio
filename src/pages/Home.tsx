75b99d1d11aca8f51c94f32ecd2767d274cc48e5
Paolo Missagia <login@paolomissagia.com>
2024-06-30 16:37:40 +0100

    added cv file and updated text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,9 +9,9 @@ export default function Home() {
             </div>
             <div className="flex flex-col gap-7 text-xl">
                 <h2>Full Stack Developer @ Mapal Group</h2>
-                <h2>Check out my personal <NavLink to="/projects" className="text-secondary underline decoration-dashed decoration-2 underline-offset-4">projects</NavLink></h2>
-                <h2>View my <NavLink to="/cv.pdf" className="text-secondary underline decoration-dashed decoration-2 underline-offset-4">CV</NavLink> for more professional details</h2>
-                <h2><NavLink to="/contact" className="text-secondary underline decoration-dashed decoration-2 underline-offset-4">Contact</NavLink> me with a message for a chat</h2>
+                <h2>Explore my personal <NavLink to="/projects" className="text-secondary underline decoration-dashed decoration-2 underline-offset-4">projects</NavLink></h2>
+                <h2>View my <a href="/PaoloMissagia_CV.pdf" target="_blank" className="text-secondary underline decoration-dashed decoration-2 underline-offset-4">CV</a> for detailed professional information</h2>
+                <h2><NavLink to="/contact" className="text-secondary underline decoration-dashed decoration-2 underline-offset-4">Contact</NavLink> me to discuss opportunities</h2>
                 <h2>Social links</h2>
                 <div className="flex flex-row gap-5">
                     <a href="https://github.com/paolomissagia" target="_blank"><FiGithub size={"1.5em"} /></a>
