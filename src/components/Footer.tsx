33366e9a730031de81efe5c515900bc3c6f21009
Paolo Missagia <login@paolomissagia.com>
2024-06-30 11:25:24 +0100

    updated styles for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const currentYear = new Date().getFullYear()
 
     return (
-        <footer className="border-t border-t-accent py-8 text-xl">
+        <footer className="border-t border-t-accent py-4 sm:py-8 text-xl">
             <div className="flex flex-row justify-between">
                 <span>copyright &#169; {currentYear}</span>
                 <span>all rights reserverd</span>
