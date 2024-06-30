446dd5b860a1f4c32330a5ac6c7557b938acb9ec
Paolo Missagia <login@paolomissagia.com>
2024-06-30 15:34:20 +0100

    shorted footer on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="border-t border-t-accent py-4 sm:py-8 text-xl">
             <div className="flex flex-col items-center sm:flex-row sm:justify-between">
                 <span>copyright &#169; {currentYear}</span>
-                <span>all rights reserverd</span>
+                <span className="hidden sm:flex">all rights reserverd</span>
             </div>
         </footer>
     )
