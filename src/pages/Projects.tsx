c6b53efe16cfe962dab3f0d2e41e4c0bd2097fb7
Paolo Missagia <login@paolomissagia.com>
2024-09-05 20:55:20 +0100

    updated the projects tab

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,29 @@
+import Project from "../components/Project"
+
 export default function Projects() {
     return (
         <section>
-            <h1>IN PROGRESS</h1>
+            <h1 className="text-4xl mb-12">Projects</h1>
+            <div className="flex flex-col gap-7">
+                <Project
+                    name="Primadonna"
+                    url="https://primadonna.vercel.app"
+                    date="17 April 2024"
+                    description="Find in-deep information about the opera world."
+                />
+                <Project
+                    name="Big Three"
+                    url="https://bigthree.vercel.app"
+                    date="6 April 2024"
+                    description="Numbers about the the tennis GOATs"
+                />
+                <Project
+                    name="Cultural Roadmap"
+                    url="https://culturalroadmap.vercel.app"
+                    date="20 November 2023"
+                    description="Interactive guides for classical music, books and films."
+                />
+            </div>
         </section>
     )
 }
