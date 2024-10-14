cc759779ba5806abb7492dd1a70d9d51a225bcf1
Paolo Missagia <login@paolomissagia.com>
2024-10-14 08:22:49 +0100

    updated footer and header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,9 @@ export default function Footer() {
     const currentYear = new Date().getFullYear()
 
     return (
-        <footer className="border-t border-t-accent py-4 sm:py-8 text-xl">
-            <div className="flex flex-col items-center sm:flex-row sm:justify-between">
-                <span>copyright &#169; {currentYear}</span>
-                <span className="hidden sm:flex">all rights reserverd</span>
-            </div>
-        </footer>
+        <footer className="flex flex-row items-center justify-center sm:justify-between py-8 text-lg">
+            <span>copyright &#169; {currentYear}</span>
+            <span className="hidden sm:flex">all rights reserverd</span>
+        </footer >
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,9 @@
 import { NavLink } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { FiMenu, FiX } from "react-icons/fi";
 
 export default function Header() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenuOpen = () => setIsMenuOpen(!isMenuOpen);
-
-    //TODO: Think in a clearner way to do it
-    useEffect(() => {
-        if (isMenuOpen) {
-            document.body.classList.add('menu-open');
-        } else {
-            document.body.classList.remove('menu-open');
-        }
-    }, [isMenuOpen]);
-
     return (
-        <header className="border-b border-b-accent text-xl flex flex-col sm:flex-row justify-between items-center">
-            <div className="flex justify-between items-center w-full sm:w-auto py-4 sm:py-8">
-                <NavLink to="/">pmissagia$</NavLink>
-                <button className="sm:hidden" onClick={toggleMenuOpen}>
-                    {isMenuOpen ? <FiX size={"1.2em"} /> : <FiMenu size={"1.2em"} />}
-                </button>
-            </div>
-            <nav className={`w-full sm:w-auto ${isMenuOpen ? 'block menu-open' : 'hidden'} sm:block`}>
-                <ul className="flex flex-col sm:flex-row gap-5 items-center sm:items-stretch py-4">
-                    <li>
-                        <NavLink to="/" onClick={() => setIsMenuOpen(false)}>home<span>/</span></NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/projects" onClick={() => setIsMenuOpen(false)}>projects<span>/</span></NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <header className="flex flex-row items-center justify-center sm:justify-between py-8 text-lg">
+            <NavLink to="/">pmissagia$</NavLink>
+        </header >
     );
 }
